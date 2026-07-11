b1600d56577682c9fad05af0903e78b79f4081d9
flyworker <ccao@swancloud.io>
2026-07-11 23:29:12 +0000

    chore: bump version to v0.3.0

--- build/version.go
+++ build/version.go
@@ -11,7 +11,7 @@ var DefaultInferenceWSURL = "wss://inference-ws.swanchain.io"
 var DefaultInferenceAPIURL = "https://api.swanchain.io/api/v1"
 var DefaultInferenceDashboardURL = "https://inference.swanchain.io"
 
-const BuildVersion = "0.2.0"
+const BuildVersion = "0.3.0"
 
 const UBITaskImageIntelCpu = "swanhub/ubi-worker-cpu-intel:latest"
 const UBITaskImageIntelGpu = "swanhub/ubi-worker-gpu-intel:latest"
